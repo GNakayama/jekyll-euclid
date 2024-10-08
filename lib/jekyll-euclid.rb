c6858fef0ed81cf92c1cdae826093c4eec160029
Guilherme <guilherme.nakayama.s@gmail.com>
2024-10-08 18:45:24 +0100

    Update module

--- lib/jekyll-euclid.rb
+++ lib/jekyll-euclid.rb
@@ -1,1 +1,8 @@
-require "jekyll-euclid/version"
+require "jekyll"
+
+require File.expand_path('jekyll/converters/euclid', File.dirname(__FILE__))
+
+module Jekyll
+  module EuclidConverter
+  end
+end
